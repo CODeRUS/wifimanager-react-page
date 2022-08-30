9dabbd5c593b250d8dd579e37cbd6b07a2ef1b31
CODeRUS <coderusinbox@gmail.com>
2022-08-30 17:23:41 +0000

    Deploying to gh-pages from @ CODeRUS/wifimanager-react-page@cf345cf760236fd7960b551007b7aae8d3e16594 🚀

---
+++ precache-manifest.c025d488cb7f3c358852fb65d1d03766.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa94c8d7d1593f18b0fb35c19d6d0ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0be915aead9b23f3f7",
+    "url": "/static/css/wifi-main.css"
+  },
+  {
+    "revision": "7f0be915aead9b23f3f7",
+    "url": "/static/js/wifi-main.js"
+  },
+  {
+    "revision": "f9b970333ed7fd9a822b75e978378505",
+    "url": "/static/js/wifi-main.js.LICENSE.txt"
+  }
+]);
